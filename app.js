65621980965a680923d0254401fa8a87b64aa85f
mohammadrz1 <127238863+mohammadrz1@users.noreply.github.com>
2023-09-06 21:34:04 +0300

    add js add btn

---
+++ app.js
@@ -0,0 +1,25 @@
+// add itm card purple
+function addBtn1(){
+    let itmCard = document.getElementById("conteant").innerHTML += " <div class='itm-card-purple' >   <div>       <div class='text-area-itm-card'>           <textarea placeholder='title'></textarea>           <textarea placeholder='text'></textarea>       </div>       <div class='itm-card-icon'>           <img src='img/itm card icon/star.svg' alt='star'>           <img src='img/itm card icon/trasch.svg' alt='star'>       </div>   </div></div> "
+    
+    return itmCard;
+}
+// add itm card red
+function addBtn2(){
+    let itmCard = document.getElementById("conteant").innerHTML += " <div class='itm-card-red' >   <div>       <div class='text-area-itm-card'>           <textarea placeholder='title'></textarea>           <textarea placeholder='text'></textarea>       </div>       <div class='itm-card-icon'>           <img src='img/itm card icon/star.svg' alt='star'>           <img src='img/itm card icon/trasch.svg' alt='star'>       </div>   </div></div> "
+    
+    return itmCard;
+}
+// add itm card blue
+function addBtn3(){
+    let itmCard = document.getElementById("conteant").innerHTML += " <div class='itm-card-blue' >   <div>       <div class='text-area-itm-card'>           <textarea placeholder='title'></textarea>           <textarea placeholder='text'></textarea>       </div>       <div class='itm-card-icon'>           <img src='img/itm card icon/star.svg' alt='star'>           <img src='img/itm card icon/trasch.svg' alt='star'>       </div>   </div></div> "
+    
+    return itmCard;
+}
+// add itm card yellow
+function addBtn4(){
+    let itmCard = document.getElementById("conteant").innerHTML += "   <div class='itm-card-yellow' >   <div>       <div class='text-area-itm-card'>           <textarea placeholder='title'></textarea>           <textarea placeholder='text'></textarea>       </div>       <div class='itm-card-icon'>           <img src='img/itm card icon/star.svg' alt='star'>           <img src='img/itm card icon/trasch.svg' alt='star'>       </div>   </div></div></div> "
+    return itmCard;
+}
+// local sorge
+localStorage.setItem("itm-card",itmCard);
